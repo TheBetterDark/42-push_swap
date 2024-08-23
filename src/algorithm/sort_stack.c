e29bc53768335479d3dee78c3c43ee747129cf4d
muabdi <91607604+TheBetterDark@users.noreply.github.com>
2024-08-23 15:46:07 +0100

    chore: Refactor sort_stack.c for better readability and maintainability

--- src/algorithm/sort_stack.c
+++ src/algorithm/sort_stack.c
@@ -6,7 +6,7 @@
 /*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 21:26:00 by muabdi            #+#    #+#             */
-/*   Updated: 2024/07/23 01:48:10 by muabdi           ###   ########.fr       */
+/*   Updated: 2024/08/23 15:45:39 by muabdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ static void	rotate_a_b(t_stack **stacks, t_stack *cheapest);
 static void	rotate_to_new_head(t_stack **stacks, t_stack *new_head,
 				char stack_name);
 
+// Push the cheapest node from stack A to stack B
 void	push_a_to_b(t_stack **stacks)
 {
 	t_stack	*cheapest;
@@ -32,12 +33,14 @@ void	push_a_to_b(t_stack **stacks)
 	push_b(stacks);
 }
 
+// Push the cheapest node from stack B to stack A
 void	push_b_to_a(t_stack **stacks)
 {
 	rotate_to_new_head(stacks, stacks[1]->target_node, 'a');
 	push_a(stacks);
 }
 
+// Set the target node for the cheapest node in stack A
 static void	rotate_a_b(t_stack **stacks, t_stack *cheapest)
 {
 	while (stacks[1] != cheapest->target_node && stacks[0] != cheapest)
@@ -46,6 +49,7 @@ static void	rotate_a_b(t_stack **stacks, t_stack *cheapest)
 	set_current_index(stacks[1]);
 }
 
+// Set the target node for the cheapest node in stack B
 static void	reverse_rotate_a_b(t_stack **stacks, t_stack *cheapest)
 {
 	while (stacks[1] != cheapest->target_node && stacks[0] != cheapest)
@@ -54,6 +58,7 @@ static void	reverse_rotate_a_b(t_stack **stacks, t_stack *cheapest)
 	set_current_index(stacks[1]);
 }
 
+// Rotate the stack to the new head
 static void	rotate_to_new_head(t_stack **stacks, t_stack *new_head,
 		char stack_name)
 {
